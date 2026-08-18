cf5bd3a800ba106e42a640df0bcf457f06610db8
vivinlara <laravivin15uco@gmail.com>
2026-08-18 18:21:05 +0530

    33

--- backend/check_cols.js
+++
@@ -1,7 +0,0 @@
-const db = require('./db');
-async function run() {
-  const res = await db.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'users'");
-  console.log(res.rows);
-  process.exit();
-}
-run();
--- backend/check_update_cols.js
+++
@@ -1,7 +0,0 @@
-const db = require('./db');
-async function run() {
-  const res = await db.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'update_requests'");
-  console.log(res.rows);
-  process.exit();
-}
-run();
